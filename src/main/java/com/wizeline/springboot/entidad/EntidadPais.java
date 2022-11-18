9d910bbbff7aa3e185b9272b9ac2ba1aecce15d8
Mario Cruz <mario.cruz@wizeline.com>
2022-11-18 10:36:09 -0600

    Adding changes in order to force Sonar error

--- src/main/java/com/wizeline/springboot/entidad/EntidadPais.java
+++ src/main/java/com/wizeline/springboot/entidad/EntidadPais.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 /**
  * @author orlando.rincon@wizeline.com
@@ -43,4 +44,17 @@ public class EntidadPais {
     public Integer obtenerPoblacion () {
         return this.poblacion;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EntidadPais that = (EntidadPais) o;
+
+        if (!Objects.equals(id, that.id)) return false;
+        if (!Objects.equals(nombre, that.nombre)) return false;
+        return Objects.equals(poblacion, that.poblacion);
+    }
+
 }
